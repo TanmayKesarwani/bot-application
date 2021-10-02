94dc1e889e4b1ff0ae795b464673df15c2f6ef90
Tanmay Kesarwani <tanmay.k@valuebound.com>
2021-10-02 09:10:06 +0530

    commmit1

--- src/features/counter/Counter.js
+++ src/features/counter/Counter.js
@@ -1,67 +1,84 @@
-import React, { useState } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import {
-  decrement,
-  increment,
-  incrementByAmount,
-  incrementAsync,
-  incrementIfOdd,
-  selectCount,
-} from './counterSlice';
-import styles from './Counter.module.css';
+import React, { useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { increment, selectCount } from "./counterSlice";
+import ourData from "../../ourData";
 
 export function Counter() {
   const count = useSelector(selectCount);
   const dispatch = useDispatch();
-  const [incrementAmount, setIncrementAmount] = useState('2');
 
-  const incrementValue = Number(incrementAmount) || 0;
+  const [detailScreenFlag, detailScreenFlagChange] = useState(false);
+  const [currentData, currentDataChange] = useState({});
+
+  function viewAlgoClick(data) {
+    currentDataChange(data);
+    detailScreenFlagChange(true);
+  }
 
   return (
     <div>
-      <div className={styles.row}>
-        <button
-          className={styles.button}
-          aria-label="Decrement value"
-          onClick={() => dispatch(decrement())}
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <div
+          className="navbar-brand"
+          onClick={() => {
+            detailScreenFlagChange(false);
+          }}
         >
-          -
-        </button>
-        <span className={styles.value}>{count}</span>
-        <button
-          className={styles.button}
-          aria-label="Increment value"
-          onClick={() => dispatch(increment())}
-        >
-          +
-        </button>
-      </div>
-      <div className={styles.row}>
-        <input
-          className={styles.textbox}
-          aria-label="Set increment amount"
-          value={incrementAmount}
-          onChange={(e) => setIncrementAmount(e.target.value)}
-        />
-        <button
-          className={styles.button}
-          onClick={() => dispatch(incrementByAmount(incrementValue))}
-        >
-          Add Amount
-        </button>
-        <button
-          className={styles.asyncButton}
-          onClick={() => dispatch(incrementAsync(incrementValue))}
-        >
-          Add Async
-        </button>
-        <button
-          className={styles.button}
-          onClick={() => dispatch(incrementIfOdd(incrementValue))}
-        >
-          Add If Odd
-        </button>
-      </div>
+          Dashboard
+        </div>
+        <div className="nav-link disabled">Cart - {count}</div>
+      </nav>
+      {detailScreenFlag ? (
+        <>
+          <div className="row">
+            <div className="col-1">
+              <label>{currentData.bot}</label>
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-5">
+              {currentData.description}
+              <br />
+              <button>MODERATE RISK</button>
+              <button onClick={() => dispatch(increment())}>Buy</button>
+            </div>
+            <div className="col-1">index-value {currentData.index_value}</div>
+            <div className="col-1">
+              CAGR <br />
+              {currentData.cagr}%
+            </div>
+          </div>
+        </>
+      ) : (
+        <div>
+          <div>Select the best algo suited for your needs</div>
+          <br />
+          <br />
+          {ourData.map((data) => {
+            return (
+              <article>
+                <div className="row">
+                  <label className="col-2">{data.bot}</label>
+                  <div className="col-1">index-value {data.index_value}</div>
+                  <div className="col-1">CAGR {data.cagr}%</div>
+                  <div className="col-1">
+                    <button
+                      onClick={() => {
+                        viewAlgoClick(data);
+                      }}
+                    >
+                      View Algo
+                    </button>
+                    <button onClick={() => dispatch(increment())}>Buy</button>
+                  </div>
+                </div>
+                <br />
+                <br />
+              </article>
+            );
+          })}
+        </div>
+      )}
     </div>
   );
 }
